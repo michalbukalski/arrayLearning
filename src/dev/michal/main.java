ae1ee5640509b181674e64d7cde5a863b36c178b
michalbukalski <michalbukalski@gmail.com>
2025-03-30 00:00:12 +0100

    Arrays.toString nowa metoda nauczona

--- src/dev/michal/main.java
+++ src/dev/michal/main.java
@@ -1,5 +1,7 @@
 package dev.michal;
 
+import java.util.Arrays;
+
 public class main {
 
     public static void main(String[] args) {
@@ -15,10 +17,11 @@ public class main {
 
 
         int[] newArray;
-        newArray = new int[] {5,4,3,2,1,0,-1};
+        newArray = new int[] {5,4,3,2,1,8,9,100,562,251,-120,0,-1};
         for (int i = 0; i< newArray.length; i++){
             System.out.println("Value of array nr " + i + " is [" + newArray[i] + "]");
 //            System.out.println("index of array is "+ i + " value of array is [" + newArray[i] + "]");
         }
+        System.out.println(Arrays.toString(newArray));
     }
 }
